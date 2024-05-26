e4a2f9a7534e0897455b90ce549166e413e8048f
Southerndave <obohdavid2014@gmail.com>
2024-05-25 17:01:13 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿BankA bankA = new BankA();
+decimal balanceA = bankA.getBalance();
+Console.WriteLine("Bank A balance is: $" + balanceA);
+
+BankB bankB = new BankB();
+decimal balanceB = bankB.getBalance();
+Console.WriteLine("Bank B balance is: $" + balanceB);
+
+BankC bankC = new BankC();
+decimal balanceC = bankC.getBalance();
+Console.WriteLine("Bank C balance is: $" + balanceC);
+
+public abstract class Bank
+{
+    public abstract decimal getBalance();
+}
+
+public class BankA : Bank
+{
+    public override decimal getBalance()
+    {
+        return 100;
+    }
+}
+
+public class BankB : Bank
+{
+    public override decimal getBalance()
+    {
+        return 150;
+    }
+}
+
+public class BankC : Bank
+{
+    public override decimal getBalance()
+    {
+        return 200;
+    }
+} 
+
+public abstract class Bank 
+{
+    public decimal getBalance();
+}
